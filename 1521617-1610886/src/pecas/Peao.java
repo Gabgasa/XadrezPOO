79807580c2e89b941547eca76036f4a436874ef3
yangurevitz <32618901+yangurevitz@users.noreply.github.com>
2018-05-19 20:39:50 -0300

    Comeco da implementacao da classe peao

---
+++ 1521617-1610886/src/pecas/Peao.java
@@ -0,0 +1,34 @@
+package pecas;
+
+import java.util.Vector;
+
+import estruturas.Pair;
+
+public class Peao extends Peca 
+{
+
+	@Override
+	public Vector<Vector<Pair<Integer, Integer>>> possiveisMovimentos() 
+	{
+		Vector<Vector<Pair<Integer, Integer>>> jogadas = new Vector<Vector<Pair<Integer, Integer>>>();
+		Pair<Integer, Integer> destino = new Pair<>(posicao.getFirst()-1, posicao.getSecond()+1);
+		Vector<Pair<Integer, Integer>> jogada = new Vector<Pair<Integer, Integer>>();
+		jogada.add(posicao);
+		jogada.add(destino);
+		if(destino.getFirst()>=0 && destino.getSecond()<=7 /* conferir se existe uma peca do mesmo jogador na casa */)
+			jogadas.add(jogada);
+		destino.setFirst(posicao.getFirst()+1);
+		jogada.setElementAt(destino, 1);
+		if(destino.getFirst()<=7 && destino.getSecond()<=7 /* conferir se existe uma peca do mesmo jogador na casa */)
+			jogadas.add(jogada);
+		return jogadas;
+	}
+
+	@Override
+	public boolean move(Pair<Integer, Integer> newPos) 
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
